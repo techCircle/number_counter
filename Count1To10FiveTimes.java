bb277a1c59b329d65ad9b438570f32f380b173f9
TechCircle,LLC <53327631+techCircle@users.noreply.github.com>
2023-05-31 19:08:29 -0400

    Create Count1To10FiveTimes.java

---
+++ Count1To10FiveTimes.java
@@ -0,0 +1,35 @@
+package day13_nestedLoop_String;
+
+public class Count1To10FiveTimes {
+
+	public static void main(String[] args) {
+
+		// count 1 to 10 five times
+
+//		for (int i = 1; i < 11; i++) {
+//			System.out.print(i+" ");
+//		}
+
+		for (int k = 1; k <= 5; k++) { // outer loop 
+			System.out.println("Hello");
+			for (int i = 1; i < 11; i++) { // inner loop
+				System.out.print(i+" "); //// 1 to 10
+			}
+			
+			System.out.println();
+			
+
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
